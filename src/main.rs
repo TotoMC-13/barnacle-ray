f571796e2df46b80ea7456f5e7e68e7d95021534
TotoMC-13 <tomasazcaray@gmail.com>
2026-03-24 18:09:09 -0300

    Comments on main and final example image

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let mut scene: Scene = Scene::random_spheres();
     let cam: &mut Camera = &mut scene.cam;
 
-    cam.image_width = 1920;
-    cam.samples_per_pixel = 400;
-    cam.defocus_angle = 0.6;
-    cam.focus_dist = 10.0;
+    cam.image_width = 400; // Use 1920 or whatever you prefer here, 400 is for fast renders.
+    cam.samples_per_pixel = 50; // Put something like 400 here for good quality.
+    cam.defocus_angle = 0.6; // Depends on your scene, put 0.0 for no blur.
+    cam.focus_dist = 10.0; // Also depends on your scene.
     scene.render();
 }
